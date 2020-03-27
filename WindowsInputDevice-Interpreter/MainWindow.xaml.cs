2bc2815723d0a6764f3f3fe006397bce38c7c0dd
jmm <josemmazzei@gmail.com>
2020-03-27 15:51:08 -0300

    Initial commit

---
+++ WindowsInputDevice-Interpreter/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows;
+
+namespace WindowsInputDevice_Interpreter
+{
+    public partial class MainWindow : Window
+    {
+        SerialPort port = new SerialPort();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            configurePort(port);
+        }
+
+        public void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dataReceived = port.ReadExisting();
+            Console.WriteLine(dataReceived);
+        }
+
+        public void connect(object obj, EventArgs e)
+        {
+            if (!port.IsOpen)
+                port.Open();
+        }
+
+        public void configurePort(SerialPort p)
+        {
+            port.PortName = "COM19";
+            port.BaudRate = 9600;
+            port.DtrEnable = true;
+            port.DtrEnable = true;
+            port.Parity = Parity.None;
+            port.DataBits = 8;
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+    }
+}
